7597ce4df03bf1070fe2f8ca72dbaca2150408f0
soonysoo <soony450@gmail.com>
2021-10-06 11:25:44 +0900

    API 사용자정보 추가

--- apiserver/index.js
+++ apiserver/index.js
@@ -3,20 +3,34 @@ var app = express(); // 할당
 var morgan = require('morgan');
 
 var users = [
-    { id:1, name:'주하'},
-    { id:2, name:'민하'},
-    { id:3, name:'세하'},
+    { id: 1, name:'주하'},
+    { id: 2, name:'민하'},
+    { id: 3, name:'세하'},
 ];
 
 app.use(morgan('dev'));
 
+
+//사용자 목록 조회 API
 app.get('/users', function (req, res) {
-  res.json(users);
+  req.query.limit = req.query.limit || 10 ;
+  const limit = parseInt(req.query.limit,10);
+  if(Number.isNaN(limit)){
+    //end()함수로 응답을 진행해준다. 
+    return res.status(400).end();
+  }
+  res.json(users.slice(0,limit));
 });
 
+//사용자 조회 API 
+app.get('/users/:id', function(req, res){
+    const id = parseInt(req.params.id, 10) //10진법 
+    const user =  users.filter((user)=> user.id === id)[0];
+    res.json(user);
+});
 
 app.listen(3040, function () {
   console.log('Example app listening on port 3030!');
 }); 
 
-module.exports = app;  //객체를 할당 
+module.exports = app;  //객체를 할당  
--- apiserver/index.spec.js
+++ apiserver/index.spec.js
@@ -1,14 +1,49 @@
 const app = require('./index');
-const request = require('supertest')
+const should =  require('should');
+const request = require('supertest');
 
 describe('GET /users는 ', () =>{
-    //두번째 함수에서 슈퍼테스트 실행시킨다
-    it('...',(done)=>{  //nodejs는 비동기로 돌기때문에 테스트가 끝나는 시점에 done을 호출해주기만 하면된다. 
-        request(app)
-            .get('/users')
-            .end((err, res)=>{
-                console.log(res.body)
-                done();
+    describe('성공시', ()=>{
+        it('유저 객체를 담은 배열로 응답한다',(done)=>{   
+            request(app)
+                .get('/users')
+                .end((err, res)=>{
+                    // 받은 배열 값을 검증한다
+                    res.body.should.be.instanceOf(Array);
+                    done();
+                })
+        })
+        it('최대 limit갯수만큼 응답한다', (done) => {
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res) => {
+                    res.body.should.have.lengthOf(2);
+                    done();
+                })
+        })
+        describe('실패시', ()=>{
+            it('limit이 숫자가 아니면 400을 응답한다',(done)=>{
+                request(app)
+                    .get('/users?limit=two')
+                    .expect(400) //400응답 값을 기대한다. 
+                    .end(done);
             })
+        })
     })
 })
+
+
+describe('GET users/1는 ', () =>{
+    describe('성공시', () =>{
+        it('id가 1인 유저 객체를 반환한다',(done) =>{
+            request(app)
+                .get('/users/1')
+                .end((err, res) => {
+                    //첫번째 파라미더 기대하는 ID , 
+                    //두번째 파리미터 기대하는 값
+                    res.body.should.have.property('id', 1);
+                    done();
+                })
+        })
+    })
+  })
