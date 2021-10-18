7e5ce3aa56e4230bffba137ddc65c2422cb39bf5
soonysoo <soony450@gmail.com>
2021-10-18 17:16:02 +0800

    api init

--- apiserver/index.js
+++ apiserver/index.js
@@ -1,76 +1,46 @@
 var express = require('express'); //모듈가지고 오기
 var app = express(); // 할당
 var morgan = require('morgan');
-
-var users = [
-    { id: 1, name:'주하'},
-    { id: 2, name:'민하'},
-    { id: 3, name:'세하'},
-];
+var fs = require('fs');
+var cors = require('cors');
+var router = express.Router();
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-//사용자 목록 조회 API
-app.get('/users', function (req, res) {
-  req.query.limit = req.query.limit || 10 ;
-  const limit = parseInt(req.query.limit,10);
-  if(Number.isNaN(limit)){
-    //end()함수로 응답을 진행해준다. 
-    return res.status(400).end();
-  }
-  res.json(users.slice(0,limit));
-});
+var corsOptions = {
+  origin : "localhost:3000",
+  credentials : true,
+}
 
-//사용자 조회 API 
-app.get('/users/:id', function(req, res){
-    const id = parseInt(req.params.id, 10) //10진법 
-    if(Number.isNaN(id)) return res.status(400).end();
+app.use(cors(corsOptions));
+let kernelData;
 
-    const user =  users.filter((user)=> user.id === id)[0];
-    if(!user) return res.status(404).end();    
-    res.json(user);
-});
-
-app.delete('/users/:id', (req, res) =>{
-  const id = parseInt(req.params.id, 10);
-  if(Number.isNaN(id)) return res.status(400).end();
-  users = users.filter(user => user.id !==id);
-  res.status(204).end();
+//kernel 정보 전달 
+app.get('/resource/kernel', function(req, res){
+  let fileData = fs.readFileSync('D:\\CTI_신한생명_TM\\Cfg\\cfg_kernel.json','utf8');
+  kernelData = JSON.parse(fileData);
+  //res.header("Access-Control-Allow-Origin", "*");
+  res.json(kernelData);
+  //res.json(kernelData);
 })
 
-app.post('/users', (req, res) => {
-  //express는 body를 지원하지 않음.
-  //bodyparser 필요
-  const name = req.body.name;
-  
-  if(!name) return res.status(400).end();
+//kernel 정보 수정
+app.post('/resource/kernel',(req, res) => {
+  const key = req.body.key;
+  const value = req.body.value;
 
-  //중복된 데이터인지 확인
-  const isConflict = users.filter(user => user.name === name).length;
-  if(isConflict) return res.status(409).end();
+  //if(!kernelData) return res.status(404).end();
+  kernelData[key] = value;
+  kernelData['update_config'] = 1;
+  console.log(kernelData);
 
-  const id = Date.now();
-  const user = {id, name};
-  users.push(user);
   res.status(201).json(user);
 })
 
-app.put('/users/:id', (req, res) =>{
-  const id  = parseInt(req.params.id , 10);
-  //id가 정수인지 아닌지
-  if(Number.isNaN(id)) return res.status(400).end();
-  const name = req.body.name;
-
-  const user = users.filter(user => user.id === id)[0];
-  user.name = name;
-  
-  res.json(user);
-})
-
 app.listen(3040, function () {
-  console.log('Example app listening on port 3030!');
+  console.log('Example app listening on port 3040!');
 }); 
 
-module.exports = app;  //객체를 할당  
+module.exports = app; 
---
+++ apiserver/index_temp.js
@@ -0,0 +1,76 @@
+var express = require('express'); //모듈가지고 오기
+var app = express(); // 할당
+var morgan = require('morgan');
+
+var users = [
+    { id: 1, name:'주하'},
+    { id: 2, name:'민하'},
+    { id: 3, name:'세하'},
+];
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+//사용자 목록 조회 API
+app.get('/users', function (req, res) {
+  req.query.limit = req.query.limit || 10 ;
+  const limit = parseInt(req.query.limit,10);
+  if(Number.isNaN(limit)){
+    //end()함수로 응답을 진행해준다. 
+    return res.status(400).end();
+  }
+  res.json(users.slice(0,limit));
+});
+
+//사용자 조회 API 
+app.get('/users/:id', function(req, res){
+    const id = parseInt(req.params.id, 10) //10진법 
+    if(Number.isNaN(id)) return res.status(400).end();
+
+    const user =  users.filter((user)=> user.id === id)[0];
+    if(!user) return res.status(404).end();    
+    res.json(user);
+});
+
+app.delete('/users/:id', (req, res) =>{
+  const id = parseInt(req.params.id, 10);
+  if(Number.isNaN(id)) return res.status(400).end();
+  users = users.filter(user => user.id !==id);
+  res.status(204).end();
+})
+
+app.post('/users', (req, res) => {
+  //express는 body를 지원하지 않음.
+  //bodyparser 필요
+  const name = req.body.name;
+  
+  if(!name) return res.status(400).end();
+
+  //중복된 데이터인지 확인
+  const isConflict = users.filter(user => user.name === name).length;
+  if(isConflict) return res.status(409).end();
+
+  const id = Date.now();
+  const user = {id, name};
+  users.push(user);
+  res.status(201).json(user);
+})
+
+app.put('/users/:id', (req, res) =>{
+  const id  = parseInt(req.params.id , 10);
+  //id가 정수인지 아닌지
+  if(Number.isNaN(id)) return res.status(400).end();
+  const name = req.body.name;
+
+  const user = users.filter(user => user.id === id)[0];
+  user.name = name;
+  
+  res.json(user);
+})
+
+app.listen(3040, function () {
+  console.log('Example app listening on port 3030!');
+}); 
+
+module.exports = app;  //객체를 할당  
